f049630a61c5649f0dcb64c15ad29fddb69c22b9
johnking <royjean339@gmail.com>
2026-02-12 11:05:03 +0100

    implemented stats incrementation

--- src/store.cpp
+++ src/store.cpp
@@ -5,17 +5,21 @@ namespace kv{
         std::unique_lock lock(m_mutex);
         std::cout << key << std::endl << value << std::endl;
         auto [it, inserted] = m_data.insert_or_assign(key, value);
+        m_stats.put_count++;
         return inserted;
     }
 
     std::optional<std::string> Store::get(const std::string& key){
         std::shared_lock lock(m_mutex);
         auto it = m_data.find(key);
+        m_stats.get_count++;
         if (it == m_data.end()){
+            m_stats.miss_count++;
             return std::nullopt;
         }
         else{
             std::string value = it->second;
+            m_stats.hit_count++;
             return value;
         }
     }
@@ -23,6 +27,7 @@ namespace kv{
     bool Store::erase(const std::string& key){
         std::unique_lock lock(m_mutex);
         std::cout << key << std::endl;
+        m_stats.erase_count++;
         return m_data.erase(key) > 0;
     }
 
